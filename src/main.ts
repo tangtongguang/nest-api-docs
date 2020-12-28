03463f333da97a1771ac9e78c9e665ad8f3f4989
t <a>
2020-12-28 22:06:01 +0800

    add redocs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedocModule, RedocOptions } from 'nestjs-redoc'
 import express from 'express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
@@ -17,6 +18,30 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
 
+  const redocOptions: RedocOptions = {
+    title: 'Hello Nest',
+    logo: {
+      url: 'https://redocly.github.io/redoc/petstore-logo.png',
+      backgroundColor: '#F0F0F0',
+      altText: 'PetStore logo'
+    },
+    sortPropsAlphabetically: true,
+    hideDownloadButton: false,
+    hideHostname: false,
+    // auth: {
+    //   enabled: true,
+    //   user: 'admin',
+    //   password: '123'
+    // },
+    tagGroups: [
+      {
+        name: 'Core resources',
+        tags: ['user'],
+      },
+    ],
+  };
+  // Instead of using SwaggerModule.setup() you call this module
+  await RedocModule.setup('/docs', app, document, redocOptions);
   await app.listen(3000);
 }
 bootstrap();
